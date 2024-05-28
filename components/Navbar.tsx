c1eda80003316d0f57cab29787a3eba97d45b39f
Brad Traversy <traversymedia@gmail.com>
2024-05-28 12:58:44 -0400

    Navbar and sidebar

---
+++ components/Navbar.tsx
@@ -0,0 +1,43 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import logo from '../img/logo.png';
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+const Navbar = () => {
+  return (
+    <div className='bg-primary dark:bg-slate-700 text-white py-2 px-5 flex justify-between'>
+      <Link href='/'>
+        <Image src={logo} alt='TraversyPress' width={40} />
+      </Link>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger className='focus:outline-none'>
+          <Avatar>
+            <AvatarImage src='https://github.com/shadcn.png' alt='@shadcn' />
+            <AvatarFallback className='text-black'>BT</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <Link href='/profile'>Profile</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link href='/auth'>Logout</Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
+
+export default Navbar;
